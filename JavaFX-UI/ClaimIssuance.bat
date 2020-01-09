0459265a0fa5ade6f630feac24678af9b599ab29
Felix Härer <felix.haerer@uni-bamberg.de>
2020-01-09 16:51:41 +0100

    JavaFX-UI

---
+++ JavaFX-UI/ClaimIssuance.bat
@@ -0,0 +1,1 @@
+start "" javaw --module-path "%~dp0\lib\javafx-sdk\lib" --add-modules javafx.controls,javafx.fxml -jar "%~dp0\ClaimIssuance.jar" %*
---
+++ JavaFX-UI/EthereumNode.bat
@@ -0,0 +1,2 @@
+                                                                              
+start "Parity Ethereum" parity --chain foundation --no-warp --mode active --tracing on --fat-db on --cache-size 2048 --pruning fast --pruning-history 1200 --min-peers=16 --max-peers=350 --no-serve-light --db-compaction ssd --auto-update none --jsonrpc-interface 127.0.0.1 --jsonrpc-port 9656 --geth
